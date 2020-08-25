1109afa98361945bf7ac808238a7d289ab0d51b9
breathxavier <67017795+breathxavier@users.noreply.github.com>
2020-08-25 23:52:09 +0200

    Update GoogleStep.java

--- src/test/java/stepdefinitions/GoogleStep.java
+++ src/test/java/stepdefinitions/GoogleStep.java
@@ -23,6 +23,7 @@ public class GoogleStep {
     public void user_searches_for_iphone() {
         googlePage.searchBox.sendKeys("iphone");
         googlePage.searchBox.submit();
+         System.out.println("ikinci commit from ruveyda");
     }
 
     @Then("verify the results has iphone")
