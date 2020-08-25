bc59a1850e69abe46dc834ae6c555de46c8c3e5a
arslannn151 <51801954+arslannn151@users.noreply.github.com>
2020-08-25 23:42:01 +0200

    Update GoogleStep.java
    gruba ismail'den selam

--- src/test/java/stepdefinitions/GoogleStep.java
+++ src/test/java/stepdefinitions/GoogleStep.java
@@ -12,6 +12,7 @@ public class GoogleStep {
     @Given("user on the google page")
     public void user_on_the_google_page() {
         Driver.getDriver().get("https://www.google.com");
+        System.out.println("ismail'den selamlar");
     }
 
     @Given("user searches for iphone")
