44758856724ad2931a2e84171d9af1d7d365a693
Ismail <arslannn151@gmail.com>
2020-08-26 01:10:20 +0200

    ismailden kendi branchine

--- src/test/java/stepdefinitions/ismail.java
+++ src/test/java/stepdefinitions/ismail.java
@@ -3,11 +3,15 @@ package stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
+import org.junit.Test;
 import pages.GooglePage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
 public class ismail {
 
-
+    @Test
+    public void ismail(){
+        System.out.printf("ismailden kendine");
+    }
 }
