f52f7318606fc25ed678e482000c3c755b24b00c
Ismail <arslannn151@gmail.com>
2020-08-26 01:03:31 +0200

    ismailden ikinci

--- src/test/java/stepdefinitions/ismail.java
+++ src/test/java/stepdefinitions/ismail.java
@@ -8,32 +8,6 @@ import utilities.ConfigReader;
 import utilities.Driver;
 
 public class ismail {
-    GooglePage googlePage = new GooglePage();
-    @Given("user on the google page")
-    public void user_on_the_google_page() {
-        Driver.getDriver().get("https://www.google.com");
-
-
-
-
-
-
-
-
-    }
-
-    @Given("user searches for hollanda")
-    public void user_searches_for_iphone() {
-        googlePage.searchBox.sendKeys("hollanda");
-        googlePage.searchBox.submit();
-    }
-
-    @Then("verify the results has hollanda")
-    public void verify_the_results_has_iphone() throws InterruptedException {
-        Thread.sleep(2000);
-        Assert.assertTrue(Driver.getDriver().getTitle().toLowerCase().contains("hollanda"));
-
-    }
 
 
 }
